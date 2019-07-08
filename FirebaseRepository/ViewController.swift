351de89ee441e206ee20294404c2969063fe6408
Andrew <alo42@georgetown.edu>
2019-07-07 21:22:40 -0400

    Configured Firebase (GoogleService-Info.plist is in .gitignore)

---
+++ FirebaseRepository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseRepository
+//
+//  Created by Andrew O'Brien on 7/7/19.
+//  Copyright © 2019 Andrew O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
